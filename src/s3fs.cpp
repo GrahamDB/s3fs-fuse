eacc4618acb5dc618ad32dbec59b544c61185d81
Graham Derryberry <g.derryberry@gmail.com>
2019-11-21 17:36:32 -0500

    Make it 8TB capacity and 8TB available

--- src/s3fs.cpp
+++ src/s3fs.cpp
@@ -2251,8 +2251,8 @@ static int s3fs_statfs(const char* _path, struct statvfs* stbuf)
   // 256T
   stbuf->f_bsize  = 0X01000000;
   stbuf->f_blocks = 0X80000000;
-  stbuf->f_bfree  = 0x10000000;
-  stbuf->f_bavail = 0x10000000;
+  stbuf->f_bfree  = 0x80000000;
+  stbuf->f_bavail = 0x80000000;
   stbuf->f_namemax = NAME_MAX;
   return 0;
 }
